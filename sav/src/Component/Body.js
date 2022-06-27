0b3bfdd2dad7c4abda819483e4798e3314ace1bc
nono <noam.curely@epitech.eu>
2022-06-27 16:29:43 +0200

    carousel product

--- sav/src/Component/Body.js
+++ sav/src/Component/Body.js
@@ -3,6 +3,8 @@ import { Component } from "react";
 import {
     Carousel,
     Accordion,
+    Card,
+    Button
 } from "react-bootstrap";
 import '../App.css';
 
@@ -10,7 +12,7 @@ export class Body extends Component {
     render() {
         return(
             <div>
-                <div id="carousel">
+                <div>
                     <Carousel>
                         <Carousel.Item>
                             <img className="d-block w-100" src="https://images.pexels.com/photos/47313/tulips-bloom-blossom-colorful-47313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" style={{ height: '500px'}} alt="First slide"/>
@@ -52,6 +54,49 @@ export class Body extends Component {
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
+                </div>
+                <div>
+                <Carousel id="carousel">
+                        <Carousel.Item id="item">
+                            <Card style={{ width: '400px'}}>
+                                <Card.Img variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pls%203000%20a1.jpg"/>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Card style={{ width: '400px' }}>
+                                <Card.Img variant="top" src="https://images.pexels.com/photos/212324/pexels-photo-212324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Card style={{ width: '400px' }}>
+                                <Card.Img variant="top" src="https://images.pexels.com/photos/212324/pexels-photo-212324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Carousel.Item>
+                    </Carousel>      
                 </div>
             </div>
         );
