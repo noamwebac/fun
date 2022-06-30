009d3310cade6ff9afbe23e485a64ea7b38a8de4
nono <noam.curely@epitech.eu>
2022-06-30 19:15:31 +0200

    carousel products

--- sav/src/Component/Body.js
+++ sav/src/Component/Body.js
@@ -15,7 +15,7 @@ export class Body extends Component {
             <div>
                 {/*Carousel présentation picture*/}
                 <div>
-                    <Carousel>
+                    <Carousel variant="grey">
                         <Carousel.Item>
                             <img className="d-block w-100" src="https://images.pexels.com/photos/47313/tulips-bloom-blossom-colorful-47313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" style={{ height: '500px'}} alt="First slide"/>
                             <Carousel.Caption>
@@ -66,10 +66,10 @@ export class Body extends Component {
                     <Carousel variant="dark" id="car">
                         <Carousel.Item>
                             <div className="d-flex justify-content-around">
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginLeft: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pls%203000%20a1.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -80,7 +80,7 @@ export class Body extends Component {
                                 <Card id="card-produits">
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/e7c3803795b6037556d07193a4244f4c.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -88,10 +88,10 @@ export class Body extends Component {
                                         <Button variant="success">Détails</Button>
                                     </Card.Body>
                                 </Card>
-                                <Card id="card-produits">
-                                        <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/phsa%2020-li%20a1-%20311706.jpg"/>
+                                <Card id="card-produits" style={{ marginRight: '150px'}}>
+                                    <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/phsa%2020-li%20a1-%20311706.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -103,10 +103,10 @@ export class Body extends Component {
                         </Carousel.Item>
                         <Carousel.Item>
                             <div className="d-flex justify-content-around">
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginLeft: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/phs%20600%20a1.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -117,7 +117,7 @@ export class Body extends Component {
                                 <Card id="card-produits">
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/phsl.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -125,10 +125,10 @@ export class Body extends Component {
                                         <Button variant="success">Détails</Button>
                                     </Card.Body>
                                 </Card>
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginRight: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/fbs.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -140,10 +140,10 @@ export class Body extends Component {
                         </Carousel.Item>
                         <Carousel.Item>
                             <div className="d-flex justify-content-around">
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginLeft: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pkga%2040-li%20a1.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -154,7 +154,7 @@ export class Body extends Component {
                                 <Card id="card-produits">
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pwsa%2012%20b1%20-%20ian%20312027.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -162,10 +162,10 @@ export class Body extends Component {
                                         <Button variant="success">Détails</Button>
                                     </Card.Body>
                                 </Card>
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginRight: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/plka%204%20a1.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -177,10 +177,10 @@ export class Body extends Component {
                         </Carousel.Item>
                         <Carousel.Item>
                             <div className="d-flex justify-content-around">
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginLeft: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/prma%2040%20li%20a1%20-%20311518.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -191,7 +191,7 @@ export class Body extends Component {
                                 <Card id="card-produits">
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pbsa%2012%20c2.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
@@ -199,10 +199,10 @@ export class Body extends Component {
                                         <Button variant="success">Détails</Button>
                                     </Card.Body>
                                 </Card>
-                                <Card id="card-produits">
+                                <Card id="card-produits" style={{ marginRight: '150px'}}>
                                     <Card.Img id="card-image" variant="top" src="https://sav03.fr/wa_p_albums/p_album_k0xj3q3h0/k0xj2swn6c24l4/pha%2012%20a1%20-%20312203.jpg"/>
                                     <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Title id="title">Card Title</Card.Title>
                                         <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
