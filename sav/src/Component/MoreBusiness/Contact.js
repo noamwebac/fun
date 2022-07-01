d85312c6ec3a258c9e89bf4530b5b7b2fe07da29
nono <noam.curely@epitech.eu>
2022-07-01 16:30:24 +0200

    contact

--- sav/src/Component/MoreBusiness/Contact.js
+++ sav/src/Component/MoreBusiness/Contact.js
@@ -15,24 +15,22 @@ export class Contact extends Component {
                 </Card>
                 <Card id="card-card-contact" style={{ border: 'none'}} className="d-flex justify-content-around">
                     <Card.Body>
-                        <h4 style={{ marginBottom: '20px'}}>Contactez nous par téléphone au 04 70 48 13 20</h4>
+                        <h4 style={{ marginBottom: '20px', textShadow: '10px 10px 10px #B9B9B9, 10px 10px 20px #B9B9B9'}}>Contactez nous par téléphone au 04 70 48 13 20</h4>
                         <Form>
                             <Form.Group>
-                                <Form.Label>Nom et Prénom</Form.Label>
+                                <Form.Label>Nom et Prénom:</Form.Label>
                                 <Form.Control id="contact-input" type="text" placeholder="Nom Prénom"/>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
+                                <Form.Label>Email address:</Form.Label>
                                 <Form.Control id="contact-input" type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                                </Form.Text>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicPhone">
-                                <Form.Label>Numéro de téléphone</Form.Label>
+                                <Form.Label>Numéro de téléphone:</Form.Label>
                                 <Form.Control id="contact-input" type="phone" placeholder="Numéro de téléphone"/>
                             </Form.Group>
                             <Form.Group className="mb-3">
+                                <Form.Label>Raison de votre demande</Form.Label>
                                 <Form.Select id="contact-input" aria-label="Default select example">
                                 <option>Raison de votre demande</option>
                                 <option value="1">SAV LIDL</option>
@@ -43,8 +41,8 @@ export class Contact extends Component {
                                 </Form.Select>
                             </Form.Group>
                             <Form.Group>
-                                <Form.Label>Votre message</Form.Label>
-                                <Form.Control style={{ width: '600px', height: '100px'}} as="textarea" type="textarea" placeholder="votre message"/>
+                                <Form.Label>Votre message:</Form.Label>
+                                <Form.Control style={{ width: '600px', height: '100px', marginBottom: '30px'}} as="textarea" type="textarea" placeholder="votre message"/>
                             </Form.Group>
                             <Button variant="success" type="submit">
                                 Submit
