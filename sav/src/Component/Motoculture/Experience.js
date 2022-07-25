0e97788a4ddb4eb4f3d61385e38ffd132e6d6f6c
nono <noam.curely@epitech.eu>
2022-07-25 19:16:09 +0200

    structure card

--- sav/src/Component/Motoculture/Experience.js
+++ sav/src/Component/Motoculture/Experience.js
@@ -7,26 +7,27 @@ import {
 export class Experience extends Component {
     render() {
         return (
-             <div>
-                <Card>
+             <div className="d-flex justify-content-around">
+                <Card id="card-structure">
                     <Card.Header><h2>Une structure dédiée</h2></Card.Header>
                     <Card.Body>
-                        <Card.Text>SAV03 possède un atelier complet et dédié avec tout le nécessaire pour réaliser l'ensemble des prestations, notre service technique reste au fait des nouveautés via les formations proposées par les plus grandes marques.</Card.Text>
+                        <Card.Text>SAV03 possède un atelier complet et dédié avec tout le nécessaire pour réaliser l'ensemble des prestations.<br/> 
+                                    Notre service technique reste au fait des nouveautés via les formations proposées par les plus grandes marques.</Card.Text>
                     </Card.Body>
                 </Card>
-                <Card>
+                <Card id="card-structure">
                     <Card.Header><h2>Des prestations uniques</h2></Card.Header>
                     <Card.Body>
                         <Card.Text>
-                            * Société de sercice après-vente pour GRIZZLY Tools,
-                            * Prestations motoculture avec plus de 30 agréments,
-                            * Nous réalisons des prises en charge sous et hors garantie pour les grandes distributions,
-                            * Prestations informatiques éco-responsable (appareils reconditionnés),
-                            * Disponible chaque jours de la semaine hors jours férié uniquement.
+                            * Société de sercice après-vente pour GRIZZLY Tools,<br/>
+                            * Prestations motoculture avec plus de 30 agréments,<br/>
+                            * Nous réalisons des prises en charge sous et hors garantie pour les grandes distributions,<br/>
+                            * Prestations informatiques éco-responsable (appareils reconditionnés),<br/>
+                            * Disponible chaque jours de la semaine hors jours férié uniquement.<br/>
                         </Card.Text>
                     </Card.Body>
                 </Card>
-                <Card>
+                <Card id="card-structure">
                     <Card.Header><h2>Une expérience évolutive</h2></Card.Header>
                     <Card.Body>
                         <Card.Text>Soucieux de la qualité de prestation, nous travaillons chaque jour à améliorer nos prestations. Vos feedbacks nous permettent d'être au plus près de vos besoins.</Card.Text>
