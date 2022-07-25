51811c99f2a244504fe92e7758ebe7d518e8b611
nono <noam.curely@epitech.eu>
2022-07-25 21:24:55 +0200

    page swap

---
+++ sav/src/Component/Motoculture/Swap.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+import {
+    Card
+} from "react-bootstrap";
+
+export class Swap extends Component {
+    render() {
+        return(
+            <div>
+                <Card>
+                    <img src="https://sav03.fr/wa_images/logo-swap-nouvelle-charte.png?v=1go4qci"/>
+                </Card>
+            </div>
+        );
+    }
+}
