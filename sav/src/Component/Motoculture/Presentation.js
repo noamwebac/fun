67c8b5aa329d804509c181140156732724699800
nono <noam.curely@epitech.eu>
2022-07-25 20:43:59 +0200

    page presentation

---
+++ sav/src/Component/Motoculture/Presentation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component } from "react";
+
+export class Presentation extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
