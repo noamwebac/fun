f1a68ab4e5062b1a26c34d39721406697882dfc1
nono <noam.curely@epitech.eu>
2022-07-25 16:30:18 +0200

    motoculture

--- sav/src/Component/Motoculture/Agrement.js
+++ sav/src/Component/Motoculture/Agrement.js
@@ -8,27 +8,31 @@ export class Agrement extends Component {
     render() {
         return(
             <div>
-            <div id="agre">
-            </div>
-                <Card>
-                    <Card.Header><h2>Profitez de nos 38 agréments et trouvez la réponse à votre problématique</h2></Card.Header>
-                    <Card.Body>
-                        <Card.Text>Nous travaillons exclusivement avec des professionnels et également avec de la pièce d'origine constructeur.</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Header><h3>Clients professionnels:</h3></Card.Header>
-                    <Card.Body>
-                        <Card.Text>Externalisez votre service après-vente Motoculture grâce à notre équipe et notre expérience, contactez nous pour plus d'informations.</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Header><h3>Clients particuliers:</h3></Card.Header>
-                    <Card.Body>
-                        <Card.Text>Vous recherchez une pièce détachée pour un appareil acheté chez nos partenaires, contactez nous et nous vous guiderons dans cette démarche.</Card.Text>
-                    </Card.Body>
-                </Card>
-                <img src="https://sav03.fr/wa_files/image2.jpg"></img>
+                <div id="agre">
+                </div>
+                <div>
+                    <Card id="agrement">
+                        <Card.Header><h4 id="title-partenaire">Profitez de nos 38 agréments et trouvez la réponse à votre problématique</h4></Card.Header>
+                        <Card.Body>
+                            <Card.Text style={{ fontSize: '15px'}}>Nous travaillons exclusivement avec des professionnels et également avec de la pièce d'origine constructeur.</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="d-flex justify-content-evenly">
+                    <Card id="card-partenaire">
+                        <Card.Header><h3>Clients professionnels:</h3></Card.Header>
+                        <Card.Body>
+                            <Card.Text>Externalisez votre service après-vente Motoculture grâce à notre équipe et notre expérience, contactez nous pour plus d'informations.</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card id="card-partenaire">
+                        <Card.Header><h3>Clients particuliers:</h3></Card.Header>
+                        <Card.Body>
+                            <Card.Text>Vous recherchez une pièce détachée pour un appareil acheté chez nos partenaires, contactez nous et nous vous guiderons dans cette démarche.</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                    <img src="https://sav03.fr/wa_files/image2.jpg"></img>
             </div>
         );
     }
