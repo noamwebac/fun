977cb2aa5bd0d37197f255f4658dbc2072552ec7
nono <noam.curely@epitech.eu>
2022-07-28 18:49:53 +0200

    carousel motoculture

--- sav/src/Pages/Motoculture.jsx
+++ sav/src/Pages/Motoculture.jsx
@@ -3,12 +3,12 @@ import { Component } from "react";
 import { Navigate } from "../Component/Navigate";
 import { Agrement } from "../Component/Motoculture/Agrement";
 import { Footer } from "../Component/Footer";
-import { Partenaire } from "../Component/Motoculture/Partenaire";
 import { Experience } from "../Component/Motoculture/Experience";
 import { Respect } from "../Component/Motoculture/Respect";
 import { Presentation } from "../Component/Motoculture/Presentation";
 import { Swap } from "../Component/Motoculture/Swap";
 import { Contact } from "../Component/MoreBusiness/Contact";
+import { CarouselPartenaire } from "../Component/Carousel/CarouselPartenaire";
 
 export class Motoculture extends Component {
     render() {
@@ -16,7 +16,7 @@ export class Motoculture extends Component {
             <div>
                 <Navigate/>
                 <Agrement/>
-                <Partenaire/>
+                <CarouselPartenaire/>
                 <Experience/>
                 <Respect/>
                 <Presentation/>
