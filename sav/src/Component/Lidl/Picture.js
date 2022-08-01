83bcb12373fbe6fa72fcabb65fb2220a41d7d96a
nono <nono@smtp.localhost.fr>
2022-08-01 16:14:35 +0200

    new page new component Lidl

---
+++ sav/src/Component/Lidl/Picture.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+import {
+    Card
+} from "react-bootstrap";
+
+export class Picture extends Component {
+    render() {
+        return(
+            <div>
+                <Card>
+                    <Card.Img style={{ height: '700px'}} src="https://images.pexels.com/photos/4503823/pexels-photo-4503823.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+                </Card>
+            </div>
+        );
+    }
+}
