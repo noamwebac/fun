a5de2f10179b528fd22bfbd2c1a37cccadf18304
nono <noam.curely@epitech.eu>
2022-08-12 16:52:58 +0200

    edit timetable

--- sav/src/Component/MoreBusiness/TimeTable.js
+++ sav/src/Component/MoreBusiness/TimeTable.js
@@ -1,54 +1,62 @@
 import React from "react";
 import { Component } from "react";
 import {
+    Card,
     Table
 } from "react-bootstrap";
 
 export class TimeTable extends Component {
     render() {
         return(
-            <Table style={{ width: '800px', marginLeft: 'auto', marginRight: 'auto', marginTop: '100px', marginBottom: '100px'}} striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Jours</th>
-                        <th>Début</th>
-                        <th>Fin</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Lundi</td>
-                        <td>8:30h</td>
-                        <td>18h</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Mardi</td>
-                        <td>8:30h</td>
-                        <td>18h</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Mercredi</td>
-                        <td>8:30h</td>
-                        <td>18h</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Jeudi</td>
-                        <td>8:30h</td>
-                        <td>18h</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Vendrdi</td>
-                        <td>8:30h</td>
-                        <td>18h</td>
-                    </tr>
-                </tbody>
-            </Table>
+            <div>
+                <Card>
+                    <Card.Header style={{ width: '400px', marginLeft: 'auto', marginRight: 'auto', marginTop: '50px'}}>Grilles horaires</Card.Header>
+                    <Table style={{ width: '800px', marginLeft: 'auto', marginRight: 'auto', marginTop: '100px', marginBottom: '100px'}} striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>Jours</th>
+                                <th>Ouverture</th>
+                                <th>Fermeture</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Lundi</td>
+                                <td>8:30h</td>
+                                <td>18h</td>
+                            </tr>
+                            <tr>
+                                <td>Mardi</td>
+                                <td>8:30h</td>
+                                <td>18h</td>
+                            </tr>
+                            <tr>
+                                <td>Mercredi</td>
+                                <td>8:30h</td>
+                                <td>18h</td>
+                            </tr>
+                            <tr>
+                                <td>Jeudi</td>
+                                <td>8:30h</td>
+                                <td>18h</td>
+                            </tr>
+                            <tr>
+                                <td>Vendrdi</td>
+                                <td>8:30h</td>
+                                <td>18h</td>
+                            </tr>
+                            <tr>
+                                <td>Samedi</td>
+                                <td colSpan={2}>Fermé</td>
+                            </tr>
+                            <tr>
+                                <td>Dimanche</td>
+                                <td colSpan={2}>Fermé</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Card>
+            </div>
         );
     }
 }
