9d8dcc69c14de5fc93ef20bb5df33e03570ea0fd
nono <noam.curely@epitech.eu>
2022-08-12 11:53:44 +0200

    test for formsupport new design

---
+++ sav/src/Component/Help/TestSupport.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+    Form
+} from "react-bootstrap";
+
+export function TestSupport() {
+    return(
+        <div>
+            <Form.Select aria-label="Default select example">
+                <option>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </Form.Select>
+        </div>
+    );
+}
