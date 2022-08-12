aa5106fa00bf1920ec856a011651ca86da3a39f7
nono <noam.curely@epitech.eu>
2022-08-12 11:29:21 +0200

    sticky security check very check

--- sav/src/Component/MoreBusiness/SecurSticky.js
+++ sav/src/Component/MoreBusiness/SecurSticky.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { Component } from "react";
+import React, { useEffect } from "react";
 import {
     Card,
     CloseButton
 } from "react-bootstrap";
+import { useState } from "react";
 
+export function SecurSticky() {
+    const [showText, setIsShow] = useState(true);
 
-export class SecurSticky extends Component {
-    render() {
-        return(
-            <div>
-                <Card id="card-sticky">
-                    <CloseButton id="button-close"/>
-                    <Card.Img id="secur-sticky" src="https://www.laplateforme.com/cms/i?o=%2Fsites%2Fdefault%2Ffiles%2Finline-images%2F3d-secure.jpg"/>
-                </Card>
-            </div>
-        );
-    }
+    return(
+        <div>
+            {showText ? <Card id="card-sticky">
+                <CloseButton onClick={() => setIsShow(!showText)} id="button-close"/>
+                <Card.Img id="secur-sticky" src="https://www.laplateforme.com/cms/i?o=%2Fsites%2Fdefault%2Ffiles%2Finline-images%2F3d-secure.jpg"/>
+            </Card> : null}
+        </div>
+    );
 }
