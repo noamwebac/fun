efb21c72bba77d9861b8dce3bb52b9aa7255d051
nono <noam.curely@epitech.eu>
2022-08-16 15:28:31 +0200

    controller for find user

--- sav/server/Controllers/User.js
+++ sav/server/Controllers/User.js
@@ -24,10 +24,45 @@ exports.create = (req, res) => {
             res.send(data);
         })
         .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Error for creating user"
-            });
+        res.status(500).send({
+            message: err.message || "Error for creating user"
         });
+    });
 };
 
+//find All user
+exports.findAll = (req, res) => {
+    const username = req.body.username;
+    var condition = username ? { username: { [Op.like]: `%${username}%` } } : null;
+
+    User.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+        res.status(500).send({
+            message: err.message || "Error for find user"
+        });
+    });
+};
+
+//find One user
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    User.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `No user exist with id=${id}`
+                });
+            }
+        })
+        .catch(err => {
+        res.status(500).send({
+            message: "Error retrieving this user whith id=" + id
+        });
+    });
+};
