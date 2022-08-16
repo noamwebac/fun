22e8f4621c3b99c5c1ccfbf62ef7496acb24cee3
nono <noam.curely@epitech.eu>
2022-08-16 15:04:26 +0200

    start server check

--- sav/server/server.js
+++ sav/server/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+const cors = require("cors");
 const app = express();
 
+var corsOptions = {
+    origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
