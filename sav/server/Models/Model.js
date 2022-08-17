4706ab4a3e90049ae72b4d330adce6377f23e9fe
nono <noam.curely@epitech.eu>
2022-08-17 15:11:38 +0200

    server role

---
+++ sav/server/Models/Model.js
@@ -0,0 +1,30 @@
+const config = require("../Config/DbConfig");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect
+    }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.Sequelize = sequelize;
+db.user = require("./UserModel")(sequelize, Sequelize);
+db.role = require("./RoleModel")(sequelize, Sequelize);
+db.role.belongTomany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+db.user.belongTomany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
