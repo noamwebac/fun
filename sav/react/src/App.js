b8b92085f4e7f61492bad3454d2f2f5cae9be5b0
nono <noam.curely@epitech.eu>
2022-08-17 11:32:57 +0200

    form

--- sav/react/src/App.js
+++ sav/react/src/App.js
@@ -4,6 +4,8 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { Provider } from "react-redux";
+//import { store } from "./Store"; 
 import { Home } from "./Pages/Home";
 import { Aide } from "../src/Pages/AIde";
 import { Motoculture } from "./Pages/Motoculture";
@@ -13,15 +15,17 @@ import { Partenaire } from "./Pages/Partenaire";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Aide" element={<Aide/>}/>
-        <Route path="/Motoculture" element={<Motoculture/>}/>
-        <Route path="/Lidl" element={<Lidl/>}/>
-        <Route path="/Partenaire" element={<Partenaire/>}/>
-      </Routes>
-    </BrowserRouter>
+    //<Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Aide" element={<Aide/>}/>
+          <Route path="/Motoculture" element={<Motoculture/>}/>
+          <Route path="/Lidl" element={<Lidl/>}/>
+          <Route path="/Partenaire" element={<Partenaire/>}/>
+        </Routes>
+      </BrowserRouter>
+    //</Provider>
   );
 }
 
--- sav/react/src/Component/Partenaire/Login.js
+++ sav/react/src/Component/Partenaire/Login.js
@@ -3,26 +3,41 @@ import {
     Form,
     Button
 } from "react-bootstrap";
+import {
+    useState
+} from "react";
 
 export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+    };
+
+    const handleSubmit = (e) => {
+        alert(email, password);
+    };
+
     return(
         <div>
             <Form style={{ width: '600px', marginLeft: 'auto', marginRight: 'auto', marginTop: '100px'}}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Label for="email">Email address</Form.Label>
+                    <Form.Control type="email" id="email" value={email} onChange={handleEmail} placeholder="Enter email"/>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label for="password">Password</Form.Label>
+                    <Form.Control type="password" id="password" value={password} onChange={handlePassword} placeholder="Password"/>
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Button onClick={handleSubmit} variant="primary" type="submit">Submit</Button>
             </Form>
 
         </div>
